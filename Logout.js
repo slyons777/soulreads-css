959038e38327b64a46800c1271674fc9df942640
Sarina L <sarinalyons@gmail.com>
2021-02-17 21:02:07 -0500

    Add files via upload

---
+++ Logout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import axios from 'axios';
+
+//material ui import
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+
+const Logout = (props) => {
+    const processLogout = async (e) => {
+        e.preventDefault();
+        const resp = await axios.post('/api/user/logout');
+        console.log(resp.data);
+        props.doLogout();
+    };
+
+    return (
+        <div className="logout-button">
+            <button className="logout-nav" onClick={processLogout}>
+                <ExitToAppIcon fontSize="large"></ExitToAppIcon>
+                <div className="sign-out">Sign Out</div>
+            </button>
+        </div >
+    );
+};
+
+export default Logout;
